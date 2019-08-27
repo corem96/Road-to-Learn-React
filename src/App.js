f9a04972a07f13e1d503db46ceb2e91b70b6dce1
jescamilla <corem96@gmail.com>
2019-08-27 16:22:53 -0600

    feat: new method for fetching api results

--- src/App.js
+++ src/App.js
@@ -6,10 +6,8 @@ const DEFAULT_QUERY = 'redux';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = `page=`;
 // const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
-
-const isSearched = searchTerm => 
-  item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends React.Component {
   constructor(props) {
@@ -21,14 +19,19 @@ class App extends React.Component {
     };
 
     this.searchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
   }
 
   componentDidMount() {
     const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+  }
 
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}${PARAM_PAGE}${page}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
@@ -38,55 +41,75 @@ class App extends React.Component {
     this.setState({ result });
   }
 
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+    event.preventDefault();
+  }
+
   onSearchChange(event) {
     this.setState({searchTerm: event.target.value});
   }
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId);
+    const updatedHits = this.state.result.hits.filter(isNotId);
     
-    this.setState({list: updatedList});
+    this.setState({
+      result: {...this.state.result, hits: updatedHits}
+    });
+    console.log(this.state.result);
   }
 
   render() {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0;
     if (!result) { return null; }
     
     return (
       <div className="App">
         <Search
           value={searchTerm}
-          onChange={this.onSearchChange}>Search by</Search>
-        <Table
-          list={result.hits}
-          pattern={searchTerm}
-          onDismiss={this.onDismiss}/>
+          onChange={this.onSearchChange}
+          onSubmit={this.onSearchSubmit}>Search</Search>
+        { result 
+          &&
+          <Table
+            list={result.hits}
+            onDismiss={this.onDismiss}/>
+        }
+        <div className="interactions">
+          <button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>More</button>
+        </div>
       </div>
     );
   }
 }
 
-const Search = ({ value, onChange, children }) => {
+const Search = ({ value, onChange, onSubmit, children }) => {
   return (
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="Input">
           {children}<input type="text"
-          onChange={onChange} value={value}
+          value={value}
+          onChange={onChange}
           placeholder="term"/>
+          <button type="submit">{children}</button>
         </div>
       </form>
     );
   }
 
-const Table = ({ list, pattern, onDismiss }) => {
+const Table = ({ list, onDismiss }) => {
   return (
     <div className="list-area">
-      {list.filter(isSearched(pattern)).map(item => 
+      {list.map(item => 
         <div className="author-list-area" key={item.objectID}>
           <div className="author">{item.author}</div>
           <div className="list-body">
-            <a href={item.url} target="_blank" className="title">{item.title}</a>
+            <a href={item.url} target="_blank"
+              className="title"
+              rel="noopener noreferrer">{item.title}</a>
             <div className="desc">comments: {item.num_comments}</div>
           </div>
           <div className="btnGroup">
